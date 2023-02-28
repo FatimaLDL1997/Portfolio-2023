31f01e549a0f81d0d934e5d82abd1361d99be401
Fatima Labade <fatima.labade@ontariotechu.net>
2023-02-28 10:17:17 -0500

    u

--- src/final/components/ImageTiles.js
+++ src/final/components/ImageTiles.js
@@ -3,22 +3,19 @@ import { Link } from "react-router-dom";
 import { projects } from "../data";
 
 const ImageTiles = () => {
-  const handleModal = (e) => {
-    console.log(e.currentTarget);
-  };
   return (
-    <section className="imageTiles-container">
+    <section className='imageTiles-container'>
       {projects.map((project, projectIndex) => {
         const { id, url, text, thumbnail, skills } = project;
         const newSkills = skills.map((skill) => `|${skill}| `).join("");
         return (
           <Link key={id} to={`/${id}`} className={`cell cell-${id}`}>
             {/* <div> */}
-            <div className="container">
-              <img className="photo" src={thumbnail} alt="" />
-              <div className="modal">
-                <div className="text">{text}</div>
-                <div className="skill-text">{newSkills}</div>
+            <div className='container'>
+              <img className='photo' src={thumbnail} alt='' />
+              <div className='modal'>
+                <div className='text'>{text}</div>
+                <div className='skill-text'>{newSkills}</div>
               </div>
             </div>
           </Link>
